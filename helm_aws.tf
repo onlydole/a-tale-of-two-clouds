20dfad4670817d8a0f4e2fd7fe0fe55c92314623
Taylor Dolezal <onlydole@gmail.com>
2020-09-21 13:40:32 -0700

    Comment out clouds

--- helm_aws.tf
+++ helm_aws.tf
@@ -1,20 +1,20 @@
-provider "helm" {
-  kubernetes {
-    load_config_file       = false
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     load_config_file       = false
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
 
-module "consul_aws" {
-  source     = "./modules/consul"
-  name       = "mesh"
-  datacenter = "aws"
-}
+# module "consul_aws" {
+#   source     = "./modules/consul"
+#   name       = "mesh"
+#   datacenter = "aws"
+# }
 
-module "vault_aws" {
-  source     = "./modules/vault"
-  name       = "vault"
-  datacenter = "aws"
-}
+# module "vault_aws" {
+#   source     = "./modules/vault"
+#   name       = "vault"
+#   datacenter = "aws"
+# }

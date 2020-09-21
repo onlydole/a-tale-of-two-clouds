89d7138c4c5ec70801597b4464cf971bda49f527
Taylor Dolezal <onlydole@gmail.com>
2020-09-21 13:58:54 -0700

    Update Kubernetes resources

--- helm_aws.tf
+++ helm_aws.tf
@@ -1,11 +1,11 @@
-provider "helm" {
-  kubernetes {
-    load_config_file       = false
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     load_config_file       = false
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
 
 # module "consul_aws" {
 #   source     = "./modules/consul"
